cfbf82c59dadea7bd279fd10bb67d7e8e0f1153a
Mikhail Temkine <miktemk@gmail.com>
2018-12-29 11:04:11 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mik-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'phaser-platformer-two-player';
+}
